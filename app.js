6b312015469fdbe8cdb4a21e23ac0cf9b89441db
Ruben Requena <rubnrequena@gmail.com>
2019-02-24 01:52:14 -0400

    remove body-parse

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,8 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
