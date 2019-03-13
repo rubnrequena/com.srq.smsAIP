218224b313a55e8c5d18a937238a2dce5a2f64ba
Ruben Requena <rubnrequena@gmail.com>
2019-03-13 02:48:03 -0400

    init fawn

--- config/mongo.js
+++ config/mongo.js
@@ -1,3 +1,3 @@
 //mongodb://dario:dario123@ds139985.mlab.com:39985/srqhermes
 //mongodb://localhost:27017/srqhermes
-module.exports.uri = process.env.mongoURI;
+module.exports.uri = process.env.mongoURI || 'mongodb://dario:dario123@ds139985.mlab.com:39985/srqhermes';
